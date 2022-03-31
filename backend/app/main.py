a58de56bc484a5f47cdf5525c45d8d27ae971d8c
Ekajto <kac.rutkowski@gmail.com>
2022-03-31 07:40:45 +0200

    authorization

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from app.api.api import api_router
+from app.core import security
+
 
 app = FastAPI()
 
 
+
+app.include_router(api_router)
+
 @app.get("/")
-def read_root():
+async def read_root(token = Depends(security.validate_token)):
     return {"Hello": "World"}
