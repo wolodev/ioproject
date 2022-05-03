16bfccebadd46c96363daf26f2c0ad42aa53287b
Wiktoria <wiecekw18@gmail.com>
2022-05-03 23:02:04 +0200

    insert data to database

--- crawler/cleaning_data.py
+++ crawler/cleaning_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 data = pd.read_csv('crawler_output.csv', sep = ";", encoding='cp1252')
 description = data['desc'].str.replace(',', '').str.split(' ')
-
+data = data.fillna(' ')
 for i in range(2463, 3031):
     data.loc[i, 'straight'] = 1 if 'Straight' in description[i] else 0
     data.loc[i, 'wavy'] = 1 if 'Wavy' in description[i] else 0
@@ -21,8 +21,8 @@ for i in range(32, 915):
 skincare_products = data[data['category'] == 'Skincare'].filter(["type","name","brand","img_link","oily","dry","normal"], axis=1)
 hair_products = data[data['category'] == 'Hair'].filter(["type","name","brand","img_link",'straight','wavy', 'curly', 'coily', 'fine', 'medium', 'thick'], axis=1)
 
-skincare_products.to_json('./products_data/skincare_products.json', orient='split', index=False)
-hair_products.to_json('./products_data/hair_products.json', orient='split', index=False)
+skincare_products.to_csv('./products_data/skincare_products.csv',index=False)
+hair_products.to_csv('./products_data/hair_products.csv',index=False)
 
 
 
